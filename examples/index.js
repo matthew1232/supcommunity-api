6a1f83c155fcc0d7ca6abb0c74bd74dc0af782d5
Local Admin <matthew.mulfingers@gmail.com>
2019-12-14 20:07:54 -0800

    Fixed .gitignore

---
+++ examples/index.js
@@ -0,0 +1,7 @@
+const SupcommunityScraper = require('../dist/api').default;
+
+const SupcommunityController = new SupcommunityScraper();
+SupcommunityController.fetchLatestWeek()
+.then(href => SupcommunityController.fetchDroplistItems(href))
+.then(console.log)
+.catch(err => console.log(err.message));
