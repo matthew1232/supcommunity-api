f673ba6c5f513a1095e5c4c3614d26d60b341b87
Carter Himmel <carter@elhnet.net>
2019-12-18 18:53:38 -0700

    feat: tsconfig, tslint and typings

---
+++ typings/index.d.ts
@@ -0,0 +1,41 @@
+
+declare module 'supcommunity-api' {
+	import { RequestInfo, RequestInit, Response } from 'node-fetch';
+	import { Agent } from 'http';
+
+// #region Classes
+	export class HttpsProxyAgent extends Agent {
+		public constructor(uri: string | { protocol?: string, host?: string, hostname?: string, port?: string });
+	}
+
+	export class SharedContext {
+		private constructor({ proxy, headers }: { proxy?: string, headers?: Headers })
+		public agent?: HttpsProxyAgent;
+		public fetch(url: RequestInfo, init?: RequestInit): Promise<Response>;
+		public headers?: Headers;
+		private setProxy(): SharedContext;
+	}
+
+	export class SupcommunityScraper {
+		private constructor({ proxy }: { proxy?: string });
+		protected _context: SharedContext;
+		public fetchLatestWeek(): Promise<string>;
+		public fetchDroplistItems(url: string): Promise<Item[]>;
+	}
+
+// #endregion
+
+// #region Typedefs
+
+	export interface Item {
+		name?: string;
+		image?: string;
+		price?: string;
+		description?: string;
+		positiveVotes?: string;
+		negativeVotes?: string;
+		votePercentage?: string;
+	}
+}
+
+// #endregion
